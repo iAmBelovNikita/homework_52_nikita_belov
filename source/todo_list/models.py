7ab9409ab5c8adc67dada5a99df9f4795b39f762
iAmBelovNikita <iambelovnikita@gmail.com>
2026-05-25 14:45:35 +0600

    Add basic model TodoTask and migrate it

--- source/todo_list/models.py
+++ source/todo_list/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+
+class TodoTask(models.Model):
+    description = models.CharField(max_length=200)
+    status = models.CharField(max_length=200)
+    date = models.DateField()
+
